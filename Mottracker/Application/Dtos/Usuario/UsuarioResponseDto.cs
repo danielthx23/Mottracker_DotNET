07f1be04795d1309963306928600eea8134bf7db
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2025-09-26 20:36:52 -0400

    fix: login

--- Mottracker/Application/Dtos/Usuario/UsuarioResponseDto.cs
+++ Mottracker/Application/Dtos/Usuario/UsuarioResponseDto.cs
@@ -1,14 +1,22 @@
+using Mottracker.Application.Dtos;
+using Mottracker.Application.Dtos.Telefone;
+using Mottracker.Application.Dtos.UsuarioPermissao;
+
 namespace Mottracker.Application.Dtos.Usuario
 {
-    public class UsuarioLoginResponseDto
+    public class UsuarioResponseDto
     {
         public int IdUsuario { get; set; }
         public string NomeUsuario { get; set; }
+        public string CPFUsuario { get; set; }
+        public string SenhaUsuario { get; set; }
+        public string CNHUsuario { get; set; }
         public string EmailUsuario { get; set; }
         public string TokenUsuario { get; set; }
         public DateTime DataNascimentoUsuario { get; set; }
         public DateTime CriadoEmUsuario { get; set; }
-        public bool LoginSucesso { get; set; }
-        public string Mensagem { get; set; }
+        public ContratoDto? ContratoUsuario { get; set; }
+        public List<TelefoneDto>? Telefones { get; set; }
+        public List<UsuarioPermissaoDto>? UsuarioPermissoes { get; set; }
     }
 }
